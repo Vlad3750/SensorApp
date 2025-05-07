f4b6ffd7e530eeeb9e949fef1ab4f82e267713d1
Vladislav Neicovcen <vladik.neicovcen@gmail.com>
2025-05-07 14:11:27 +0200

    Added Dockpanel to Mainwindow

---
+++ SensorApp/SensorApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SensorApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
