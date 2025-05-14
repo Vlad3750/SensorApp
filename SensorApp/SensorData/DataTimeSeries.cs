eb6c4225bc6decaf66d4fbd792ff5047209ebb00
talha <talha@TALHA_ZENGIN>
2025-05-14 14:37:14 +0200

    added Lib & Classes, added References

---
+++ SensorApp/SensorData/DataTimeSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SensorLib
+{
+    internal class DataTimeSeries
+    {
+        public List<SensorData> dataList;
+
+        public DataTimeSeries(SensorData sensorData) 
+        {
+            dataList.Add(sensorData);
+        }
+
+        public void SaveToCsv(string filePath)
+        {
+
+        }
+
+        public void LoadFromCsv(string filePath)  // <- ReturnTyp to List<SensorData>
+        {
+
+        }
+
+        public void ShowDataList(ListView list)
+        {
+
+        }
+    }
+}
--- SensorApp/SensorData/SensorData.cs
+++ SensorApp/SensorData/SensorData.cs
@@ -1,8 +1,40 @@
 
+using System.Windows.Controls;
+using System.Windows.Documents.Serialization;
+
 namespace SensorLib
 {
     public class SensorData
     {
+        public double temp { get; set; }
+
+        public double xAxis { get; set; }
+
+        public double yAxis { get; set; }
+
+        public double zAxis { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public SensorData()
+        {
+
+        }
+
+        public void SerializeToCsv()
+        {
+
+        }
+
+        public void Deserialize()   // <- ReturnTyp to SensorData
+        {
+
+        }
+
+        public void DrawCanvas(Canvas canvas)
+        {
+
+        }
     }
 
 }
